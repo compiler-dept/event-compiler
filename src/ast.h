1e8223e310ad3cfd069a2d3e0a9dee6cc17d8605
Andreas Pfohl <mail@apfohl.com>
2015-02-18 14:59:17 +0100

    Changed rule declaration type to name

--- src/ast.h
+++ src/ast.h
@@ -103,7 +103,7 @@ struct payload {
             char *identifier;
         } parameter;
         struct {
-            char *type;
+            char *name;
             char *identifier;
         } rule_declaration;
         struct {
